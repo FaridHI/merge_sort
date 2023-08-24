ffb4d4fe7e912290aa976ae64f8493aa128f8905
FaridHI <1121041@senati.pe>
2023-08-24 15:42:53 -0500

    primer commit

---
+++ merge_sort/src/main/java/com/mycompany/merge_sort/Merge_sort.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.merge_sort;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Merge_sort {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
