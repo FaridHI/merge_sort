edb46d151196b2ca5cef334b3f3045269610df03
FaridHI <1121041@senati.pe>
2023-08-24 16:16:06 -0500

    cree un while(bucle) ydos condiciones

--- merge_sort/src/main/java/com/mycompany/merge_sort/Merge_sort.java
+++ merge_sort/src/main/java/com/mycompany/merge_sort/Merge_sort.java
@@ -24,5 +24,16 @@ public class Merge_sort {
         for (int j = 0; j < n2; j++){
             rightArray[j] = arr[middle + 1 + j];
         }
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2){
+            if (leftArray[i] <= rightArray[j]){
+                arr[k] = leftArray[i];
+                i++;
+            }else{
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
     }
 }
